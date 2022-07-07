834868260f0faaec526e0247ce6bb03a49e2a23b
20014027 <20014027@rp.edu.sg>
2022-07-07 22:56:14 +0800

    update 1

--- app/src/main/java/sg/edu/rp/c346/id20014027/demosimplesave/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20014027/demosimplesave/MainActivity.java
@@ -2,6 +2,7 @@ package sg.edu.rp.c346.id20014027.demosimplesave;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -14,10 +15,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        //Step 1: Obtain the SharedPreferences instance
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        //Step 2: Create a SharedPreferences Editor by calling edit()
+        SharedPreferences.Editor prefEdit = prefs.edit();
+        //Step 3: Set a key-value pair in the editor
+        prefEdit.putString("greetings", "Hello!");
+        //Step 4: Call commit() to save the changes made to the SharedPreferences
+        prefEdit.commit();
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
-
-        String msg = "No greetings";
+        //Step 1: Obtain the SharedPreferences instance
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        //Step 2: Retrieve the saved data from the SharedPreferences
+        // with a default value if no matching data
+        String msg = prefs.getString("greetings", "No greetings name");
         Toast toast = Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG);
         toast.show();
     }
